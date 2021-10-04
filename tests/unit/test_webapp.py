ca896ddc539fd922768ba3dde34b35c0866c8345
Israel Yago Pereira <israelyago@e.email>
2021-10-04 13:16:01 -0300

    Revert "Fix other tests"
    This reverts commit 47e2e09398ff12c0057a985e6ba1898aeaae05dd.

--- tests/unit/test_webapp.py
+++ tests/unit/test_webapp.py
@@ -74,9 +74,7 @@ class ViewsTestCase(SearxTestCase):
     def test_index_empty(self):
         result = self.app.post('/')
         self.assertEqual(result.status_code, 200)
-        self.assertIn(b'<div class="text-hide center-block" id="main-logo">'
-                      + b'<img class="center-block img-responsive" src="/static/themes/etheme/img/logo_searx_a.png"'
-                      + b' alt="searx logo" />searx</div>', result.data)
+        self.assertIn(b'<img src="/static/themes/etheme/img/logo_searx_a.png" alt="/e/ logo">', result.data)
 
     def test_index_html_post(self):
         result = self.app.post('/', data={'q': 'test'})
