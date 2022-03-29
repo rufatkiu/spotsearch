770a39226a4899a193af33ccfa9163f7e0ddc17a
Nivesh Krishna <nivesh@e.email>
2022-03-29 14:03:09 +0530

    fix pep8 issues

--- searx/plugins/calculator.py
+++ searx/plugins/calculator.py
@@ -4,15 +4,17 @@ import math
 name = "Calculator"
 # TODO: translate the following line
 description = 'This plugin extends the suggestions with the word "example"'
-default_on = True 
+default_on = True
 logger = logger.getChild("calculator")
 ALLOWED_NAMES = {
     k: v for k, v in math.__dict__.items() if not k.startswith("__")
 }
 
+
 def check_if_loaded():
     logger.debug("initializing calculator plugin")
-    
+
+
 def post_search(request, search):
     if search.search_query.pageno > 1:
         return True
@@ -26,16 +28,16 @@ def post_search(request, search):
             search.result_container.answers.clear()
             # TODO: translate the following line
             answer = "The value of {} is {}".format(search.search_query.query, code)
-            search.result_container.answers[answer] =  {'answer': str(answer)}
+            search.result_container.answers[answer] = {'answer': str(answer)}
     except (ZeroDivisionError, OverflowError, ValueError, FloatingPointError, MemoryError) as e:
         logger.debug(e)
         # TODO: translate the following line
-        search.result_container.answers[f'Please recheck the above query: {e}'] =  {'answer': None}
+        search.result_container.answers[f'Please recheck the above query: {e}'] = {'answer': None}
         return False
     except (SyntaxError, NameError, TypeError) as e:
         logger.debug(e)
         # TODO: translate the following line
-        search.result_container.answers[f'Please recheck syntax of above query'] =  {'answer': None}
+        search.result_container.answers[f'Please recheck syntax of above query'] = {'answer': None}
         return False
     except Exception as e:
         logger.debug(e)
