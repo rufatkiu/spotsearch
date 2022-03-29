4547f73e21b01c2ed2182039453501ac08eed33d
Nivesh Krishna <nivesh@e.email>
2022-03-30 01:12:49 +0530

    use numexpr for evaluation of expressions

--- searx/plugins/calculator.py
+++ searx/plugins/calculator.py
@@ -1,38 +1,52 @@
 from searx import logger
-import math
+from numexpr import evaluate
 
 name = "Calculator"
 # TODO: translate the following line
-description = 'This plugin extends the suggestions with the word "example"'
+description = 'This plugin extends results when the query is a mathematical expression'
 default_on = True
 logger = logger.getChild("calculator")
-ALLOWED_NAMES = {
-    k: v for k, v in math.__dict__.items() if not k.startswith("__")
-}
 
 
 def check_if_loaded():
     logger.debug("initializing calculator plugin")
 
 
+def is_really_big(query):
+    # For cases like 2**99999**9999
+    if len(query.split("**")) >= 3:
+        return True
+    # Add more cases if needed
+    return False
+
+
 def post_search(request, search):
     if search.search_query.pageno > 1:
         return True
     try:
-        code = compile(search.search_query.query, "<string>", "eval")
-        for name in code.co_names:
-            if name not in ALLOWED_NAMES:
-                return False
-        code = eval(code, {"__builtins__": {}}, ALLOWED_NAMES)
+        # Not going to compute the result if the query is too big
+        if len(search.search_query.query) > 20:
+            return True
+        
+        # Not going to compute the result if the query is not within permissible range
+        if is_really_big(search.search_query.query):
+            raise OverflowError
+        
+        code = evaluate(search.search_query.query).item()
         if type(code) in (int, float):
             search.result_container.answers.clear()
             # TODO: translate the following line
             answer = "The value of {} is {}".format(search.search_query.query, code)
             search.result_container.answers[answer] = {'answer': str(answer)}
-    except (ZeroDivisionError, OverflowError, ValueError, FloatingPointError, MemoryError) as e:
+    except (ZeroDivisionError, ValueError, FloatingPointError, MemoryError) as e:
         logger.debug(e)
         # TODO: translate the following line
         search.result_container.answers[f'Please recheck the above query: {e}'] = {'answer': None}
+        return False
+    except OverflowError as e:
+        logger.debug(e)
+        # TODO: translate the following line
+        search.result_container.answers[f'Please recheck the above query: Too big to compute {e}'] = {'answer': None}
         return False
     except (SyntaxError, NameError, TypeError) as e:
         logger.debug(e)
