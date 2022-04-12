f0b4a54b22166e4ed28f48bd54dd19ee1f3f5175
Nivesh Krishna <nivesh@e.email>
2022-04-12 15:04:41 +0530

    set timeout to calculator

--- searx/plugins/calculator.py
+++ searx/plugins/calculator.py
@@ -1,7 +1,7 @@
 from searx import logger
 from numexpr import evaluate
 from flask_babel import gettext
-
+from wrapt_timeout_decorator import timeout
 
 name = gettext('Calculator')
 description = gettext('This plugin extends results when the query is a mathematical expression')
@@ -13,13 +13,10 @@ def check_if_loaded():
     logger.debug("initializing calculator plugin")
 
 
-def is_really_big(query):
-    # For cases like 2**99999**9999
-    if len(query.split("**")) >= 3:
-        return True
-    # Add more cases if needed
-    return False
-
+# Set timeout so that the plugin doesn't hang for long computations
+@timeout(5)
+def calculate(query):
+    return evaluate(query).item()
 
 def post_search(request, search):
     if search.search_query.pageno > 1:
@@ -45,16 +42,12 @@ def post_search(request, search):
         if len(query) > 30:
             return
 
-        # Not going to compute the result if the query is not within permissible range
-        if is_really_big(query):
-            raise OverflowError
-
-        value = evaluate(query).item()
+        value = calculate(query)
         if type(value) in (int, float):
             search.result_container.answers.clear()
             answer = "{} = {}".format(unmodified_query, value)
             search.result_container.answers[answer] = {'answer': answer, 'calculator': True}
-    except (ZeroDivisionError, ValueError, FloatingPointError, MemoryError, OverflowError) as e:
+    except (ZeroDivisionError, ValueError, FloatingPointError, MemoryError, OverflowError, TimeoutError) as e:
         answer = gettext('Error')
         search.result_container.answers[answer] = {'answer': answer, 'calculator': True}
     except Exception as e:
