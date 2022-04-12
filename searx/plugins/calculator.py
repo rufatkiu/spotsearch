5f0de8f98c6f58966bae93b37f8f8cde0742be63
Nivesh Krishna <nivesh@e.email>
2022-04-12 12:15:58 +0000

    Fix calculator

--- searx/plugins/calculator.py
+++ searx/plugins/calculator.py
@@ -1,7 +1,7 @@
 from searx import logger
 from numexpr import evaluate
 from flask_babel import gettext
-
+from wrapt_timeout_decorator import timeout
 
 name = gettext('Calculator')
 description = gettext('This plugin extends results when the query is a mathematical expression')
@@ -13,12 +13,10 @@ def check_if_loaded():
     logger.debug("initializing calculator plugin")
 
 
-def is_really_big(query):
-    # For cases like 2**99999**9999
-    if len(query.split("**")) >= 3:
-        return True
-    # Add more cases if needed
-    return False
+# Set timeout so that the plugin doesn't hang for long computations
+@timeout(5)
+def calculate(query):
+    return evaluate(query).item()
 
 
 def post_search(request, search):
@@ -27,8 +25,11 @@ def post_search(request, search):
     try:
         query = search.search_query.query.lower()
         unmodified_query = query
+
+        # Replace all frequently used substitutes
         query = query.replace("x", "*")
         query = query.replace("^", "**")
+        query = query.replace("%", "*0.01")
 
         # Not going to compute if only one number is present
         try:
@@ -45,18 +46,18 @@ def post_search(request, search):
         if len(query) > 30:
             return
 
-        # Not going to compute the result if the query is not within permissible range
-        if is_really_big(query):
-            raise OverflowError
+        # Multiply by float to upcast all numbers to floats
+        # https://numexpr.readthedocs.io/projects/NumExpr3/en/latest/user_guide.html#casting-rules
+        query += "*1.0"
 
-        value = evaluate(query).item()
+        value = calculate(query)
         if type(value) in (int, float):
             search.result_container.answers.clear()
             answer = "{} = {}".format(unmodified_query, value)
-            search.result_container.answers[answer] = {'answer': answer, 'calculator': True}
-    except (ZeroDivisionError, ValueError, FloatingPointError, MemoryError, OverflowError) as e:
+            search.result_container.answers['calculator'] = {'answer': answer, 'calculator': True}
+    except (ZeroDivisionError, ValueError, FloatingPointError, MemoryError, OverflowError, TimeoutError) as e:
         answer = gettext('Error')
-        search.result_container.answers[answer] = {'answer': answer, 'calculator': True}
+        search.result_container.answers['calculator'] = {'answer': answer, 'calculator': True}
     except Exception as e:
         logger.debug(e)
 
