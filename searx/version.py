e4522d4f850104aa8bef9249ae79f6970895c4df
Nivesh Krishna <nivesh@e.email>
2023-02-17 16:20:14 +0530

    fix lint

--- searx/version.py
+++ searx/version.py
@@ -11,6 +11,7 @@ import logging
 # if there is searx.version_frozen module, and it is not possible to get the git tag
 VERSION_STRING = "1.0.0"
 VERSION_TAG = "1.0.0"
+DOCKER_TAG = "1.0.0"
 GIT_URL = "unknow"
 GIT_BRANCH = "unknow"
 
